7bc63e0c4eeaae900c6fd83251a0996c7e30c0e0
HelloJon29 <jonkim1996@gmail.com>
2023-03-19 03:17:34 -0500

    Added useState to App.js, removed passed prop value from Square and instead used state to save value to each square component rendered in Board

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -1,27 +1,37 @@
 import "./styles.css";
+import { useState } from "react";
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null); // Here value is used to store the state, and setValue can be used to update it, null is used as the initial value of the state in this case
+  function handleClick() {
+    setValue("X"); // using setValue from above, this will tell React to rerender the element clicked
+  }
+
   //{value} is a prop that the child(Square) gets from the parent(Board)
-  return <button className="square">{value}</button>;
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={"1"} />
-        <Square value={"2"} />
-        <Square value={"3"} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={"4"} />
-        <Square value={"5"} />
-        <Square value={"6"} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={"7"} />
-        <Square value={"8"} />
-        <Square value={"9"} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
